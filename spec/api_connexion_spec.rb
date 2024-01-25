a04cada6ea4f2dc1c94755ef14376ab74665fdb2
Annie Hérieau <148761809+annieherieau@users.noreply.github.com>
2024-01-25 09:19:24 +0100

    Delete spec/api_connexion_spec.rb

--- spec/api_connexion_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require_relative '../lib/api_connexion'
-
-describe "the login_openai method" do
-  it "should return response, and response is not nil" do
-    expect(login_openai).not_to be_nil
-  end
-end
