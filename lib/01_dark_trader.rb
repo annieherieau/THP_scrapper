6adc294b41f02694c55bd356f4464f521785baaa
annieherieau <annie.herieau@gmail.com>
2024-01-25 14:04:32 +0100

    01 rspec ok

--- lib/01_dark_trader.rb
+++ lib/01_dark_trader.rb
@@ -1,21 +1,23 @@
 # frozen_string_literal: true
-# Gem PRY : outil de debuggage
-require 'pry' # Appelle la gem Pry : use binding.pry
-# gem Nokogori : scrapping
-require 'nokogiri'
-# gem open-uri : ouvrir une URL
-require 'open-uri'
-#______ keep the code above in each in project files lib/*.rb
+  # Gem PRY : outil de debuggage
+  require 'pry' # Appelle la gem Pry : use binding.pry
+  # gem Nokogori : scrapping
+  require 'nokogiri'
+  # gem open-uri : ouvrir une URL
+  require 'open-uri'
+  #______ keep the code above in each in project files lib/*.rb
 
 
-# Ouvrir la page >> returns
+# Ouvrir la page >> returns Nokogiri::HTML4::Document
+  # rspec ok
 def open_page(url)
   page = Nokogiri::HTML(URI.open(url))
   return page
 end
 
-
-# 1) Isoler les éléments HTML >> returns Hash of arrays {names: [n1, n2], rates: [r1, r2]}
+# Isoler les éléments HTML 
+  # >> returns Hash of arrays {names: [n1, n2], rates: [r1, r2]}
+  # rspec ok
 def parse_html_elements(page, xpath_elements)
   cryto_data = {
      # array noms de crytos
@@ -28,9 +30,9 @@ def parse_html_elements(page, xpath_elements)
   return cryto_data
 end
 
-
-# 3) Réorganiser ce hash dans un array 
-# returns Array of Hashes = [{name1 : rate1},{name2 : rate2}]
+# Réorganiser ce hash dans un array 
+  # returns Array of Hashes = [{name1 : rate1},{name2 : rate2}]
+  # rspec ok
 def build_scrapping_array(hash)
   array = Array.new
   i=0
@@ -38,6 +40,7 @@ def build_scrapping_array(hash)
     array.push ({name => hash[:rates][i]})
     i +=1
   end
+  puts array[0].values.nil?
   return array
 end
 
@@ -59,14 +62,13 @@ def dark_trade_scrapper(url)
     rate: '//a[contains(@href, "#markets")]//span'}
   
   # Isoler les éléments HTML (Hash of arrays)
-  cryto_data = parse_html_elements(page, xpath_elements)
-  
+  crypto_data = parse_html_elements(page, xpath_elements)
+  puts crypto_data[:rates][0].nil?
   # organiser les données
-  return build_scrapping_array(cryto_data)
+  return build_scrapping_array(crypto_data)
 end
 
 
-
-
 #________ PERFORM
+# dark_trade_scrapper("https://coinmarketcap.com/all/views/all/")
 print_results(dark_trade_scrapper("https://coinmarketcap.com/all/views/all/"))
--- spec/01_dark_trader_spec.rb
+++ spec/01_dark_trader_spec.rb
@@ -2,7 +2,33 @@
 
 require_relative '../lib/01_dark_trader'
 
-describe "function" do
-  it "returns " do
-    expect().to eq()
+url ="https://coinmarketcap.com/all/views/all/"
+page = open_page(url)
+xpath_elements = {
+  name: '//a[@class="cmc-table__column-name--name cmc-link"]/@title', 
+  rate: '//a[contains(@href, "#markets")]//span'}
+crypto_data = parse_html_elements(page, xpath_elements)
+#___________
+
+describe "open_page(url) function" do
+  it "returns a Nokogiri::HTML4::Document" do
+    expect(open_page(url).nil?).to eq(false)
+    expect(open_page(url).class).to eq(Nokogiri::HTML4::Document)
   end
+end
+
+describe "parse_html_elements(page, xpath_elements) function" do
+  it "returns a hash of arrays" do
+    expect(parse_html_elements(page, xpath_elements).class).to eq(Hash)
+    expect(parse_html_elements(page, xpath_elements)[:names][0] ).to eq("Bitcoin")
+    expect(parse_html_elements(page, xpath_elements)[:rates][0].nil?).to eq(false)
+  end
+end
+
+describe "build_scrapping_array(hash) function" do
+  it "returns a array of hash" do
+    expect(build_scrapping_array(crypto_data).length > 3).to eq(true)
+    expect(build_scrapping_array(crypto_data)[0].keys ).to eq(Bitcoin)
+    expect(build_scrapping_array(crypto_data)[0].values.nil? ).to eq(false)
+  end
+end
