90024faac2a9d77766a90177d77f89ac376ab324
annieherieau <annie.herieau@gmail.com>
2024-01-25 09:43:28 +0100

    new files

---
+++ lib/03_deputes.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+# Gem PRY : outil de debuggage
+require 'pry' # Appelle la gem Pry : use binding.pry
+# gem Nokogori : scrapping
+require 'nokogiri'
+# gem open-uri : ouvrir une URL
+require 'open-uri'
+#______ keep the code above in each in project files lib/*.rb
